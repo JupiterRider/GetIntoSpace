7287c466392c69dfef3ded6142e2b45aca82690b
JupiterRider <jupiter.rider@outlook.de>
2020-11-15 14:56:20 +0100

    Sky background improvement

--- core/src/main/java/com/github/jupiterrider/getintospace/FirstScreen.java
+++ core/src/main/java/com/github/jupiterrider/getintospace/FirstScreen.java
@@ -2,7 +2,9 @@ package com.github.jupiterrider.getintospace;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -19,7 +21,9 @@ public class FirstScreen extends ScreenAdapter {
 	private final BitmapFont bitmapFont;
 	private final Texture texture;
 	private final Sprite skySprite;
-	private float skyOffset = 0f;
+	private final Camera camera;
+	
+	private int skyOffset = 0;
 
 	public FirstScreen() {
 		rocket = new Rocket();
@@ -29,32 +33,31 @@ public class FirstScreen extends ScreenAdapter {
 
 		skySprite = new Sprite(texture);
 		skySprite.flip(true, true);
+		
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
 	public void render(float delta) {
-//		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1f);
 		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		renderBackground(delta);
-		rocket.draw(batch, delta);
-		String strFps = "FPS: " + String.valueOf(Gdx.graphics.getFramesPerSecond());
-		bitmapFont.draw(batch, strFps, 0f, Gdx.graphics.getHeight());
+		rocket.draw(batch, delta, camera);
+//		String strFps = "FPS: " + String.valueOf(Gdx.graphics.getFramesPerSecond());
+//		bitmapFont.draw(batch, strFps, 0f, Gdx.graphics.getHeight());
 		batch.end();
 	}
 
 	private void renderBackground(float delta) {
-		skyOffset += 55f * delta;
+		skyOffset = (int) (camera.position.y / Gdx.graphics.getHeight());
 
-		if ((int)skyOffset / Gdx.graphics.getHeight() == 1) {
-			skyOffset = 0f;
-		}
-
-		batch.draw(skySprite, 0f, -skyOffset, texture.getWidth() * 5, texture.getHeight() * 5);
-		batch.draw(skySprite, 0f, -skyOffset + Gdx.graphics.getHeight(), texture.getWidth() * 5,
-				texture.getHeight() * 5);
+		batch.draw(skySprite, 0f, Gdx.graphics.getHeight() * (skyOffset + 1), texture.getWidth() * 5, texture.getHeight() * 5);
+		batch.draw(skySprite, 0f, Gdx.graphics.getHeight() * skyOffset, texture.getWidth() * 5, texture.getHeight() * 5);
+		batch.draw(skySprite, 0f, Gdx.graphics.getHeight() * (skyOffset - 1), texture.getWidth() * 5, texture.getHeight() * 5);
+		batch.draw(skySprite, 0f, Gdx.graphics.getHeight() * (skyOffset - 2), texture.getWidth() * 5, texture.getHeight() * 5);
 	}
 
 	@Override
--- core/src/main/java/com/github/jupiterrider/getintospace/Rocket.java
+++ core/src/main/java/com/github/jupiterrider/getintospace/Rocket.java
@@ -2,6 +2,7 @@ package com.github.jupiterrider.getintospace;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -24,7 +25,7 @@ public class Rocket {
 	private final Vector2 position = new Vector2(Gdx.graphics.getWidth() / 2f - TEXTURE.getWidth() * 2.5f, 0f);
 	private float velocity = 0f;
 
-	public void draw(Batch batch, float delta) {
+	public void draw(Batch batch, float delta, Camera camera) {
 		if (Gdx.input.isKeyPressed(Keys.SPACE)) {
 			velocity += SPEED_MULTIPLIER * delta;
 			velocity = MathUtils.clamp(velocity, -SPEED_MAX, SPEED_MAX);
@@ -35,9 +36,12 @@ public class Rocket {
 			velocity -= SPEED_MULTIPLIER * delta;
 			velocity = MathUtils.clamp(velocity, -SPEED_MAX, SPEED_MAX);
 			position.mulAdd(new Vector2(0f, velocity), delta);
-			batch.draw(OFF_TEXTURE_REGION, position.x, position.y, ON_TEXTURE_REGION.getRegionWidth() * 5,
-					ON_TEXTURE_REGION.getRegionHeight() * 5);
+			batch.draw(OFF_TEXTURE_REGION, position.x, position.y, OFF_TEXTURE_REGION.getRegionWidth() * 5,
+					OFF_TEXTURE_REGION.getRegionHeight() * 5);
 		}
+
+		camera.position.set(position.x + TEXTURE.getWidth() * 2.5f, position.y + TEXTURE.getHeight() * 1.25f, 0f);
+		camera.update();
 	}
 
 	public static final void dispose() {
