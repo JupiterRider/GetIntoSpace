3484099131b1538b2e8ecd0659eed5337f850ff8
JupiterRider <jupiter.rider@outlook.de>
2020-11-22 18:49:22 +0100

    Issue #1 fixed

--- core/src/main/java/com/github/jupiterrider/getintospace/SaturnFive.java
+++ core/src/main/java/com/github/jupiterrider/getintospace/SaturnFive.java
@@ -14,11 +14,11 @@ public class SaturnFive {
 	private static final AtlasRegion OFF_ATLAS_REGION = AssetHandler.TEXTURE_ATLAS.findRegion("saturn5off");
 	private static final Vector2 SIZE = new Vector2(ON_ATLAS_REGION.getRegionWidth() * 5,
 			ON_ATLAS_REGION.getRegionHeight() * 5);
-	private static final float SPEED = 200f;
+	private static final float SPEED = 250f;
 	private static final Vector2 NORMAL = new Vector2(1f, 1f).nor();
 
 	private Vector2 pos;
-	private boolean up = false;
+//	private boolean up = false;
 	private boolean left = false;
 	private boolean right = false;
 	private Rectangle rectangle;
@@ -39,10 +39,10 @@ public class SaturnFive {
 			pos.mulAdd(new Vector2(SPEED, SPEED).scl(NORMAL), delta);
 			pos = clampPos(pos);
 			batch.draw(ON_ATLAS_REGION, pos.x, pos.y, SIZE.x, SIZE.y);
-		} else if (up) {
-			pos.mulAdd(new Vector2(0f, SPEED), delta);
-			pos = clampPos(pos);
-			batch.draw(ON_ATLAS_REGION, pos.x, pos.y, SIZE.x, SIZE.y);
+//		} else if (up) {
+//			pos.mulAdd(new Vector2(0f, SPEED), delta);
+//			pos = clampPos(pos);
+//			batch.draw(ON_ATLAS_REGION, pos.x, pos.y, SIZE.x, SIZE.y);
 		} else {
 			pos.mulAdd(new Vector2(0f, -100f), delta);
 			pos = clampPos(pos);
@@ -53,11 +53,11 @@ public class SaturnFive {
 	}
 
 	private void checkInput() {
-		if (Gdx.input.isKeyPressed(Keys.SPACE) || Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP)) {
-			up = true;
-		} else {
-			up = false;
-		}
+//		if (Gdx.input.isKeyPressed(Keys.SPACE) || Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP)) {
+//			up = true;
+//		} else {
+//			up = false;
+//		}
 
 		if (Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT)) {
 			left = true;
